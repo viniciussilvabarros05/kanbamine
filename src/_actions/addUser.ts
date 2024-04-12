27f1cae14b005bea796021e2d4e212bdde65f1df
viniciussilvabarros05 <viniciussilvabarros05@hotmail.com>
2024-04-12 11:49:22 -0300

    feat: integrate fireabase

---
+++ src/_actions/addUser.ts
@@ -0,0 +1,17 @@
+"use server"
+
+import { db } from "@/_firebase/config";
+import { addDoc, collection } from "firebase/firestore";
+
+export async function addUser() {
+    try {
+      const docRef = await addDoc(collection(db, "users"), {
+        first: "Ada",
+        last: "Lovelace",
+        born: 1815,
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  }
